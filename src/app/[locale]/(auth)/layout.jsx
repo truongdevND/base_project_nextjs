db090abe44693283cebb2e5e80070296e1d7ea9c
truongdevND <truong2708.dev@gmail.com>
2024-11-27 21:36:05 +0700

    update

--- src/app/[locale]/(auth)/layout.jsx
+++ src/app/[locale]/(auth)/layout.jsx
@@ -1,9 +1,11 @@
-import AuthLayout from "../../layouts/Authlayout"
-
+import AuthLayout from "../layouts/Authlayout"
+//import DefaultLayout from "@/app/layouts/DefaultLayout"
 export default function layout({children}) {
     return (
         <AuthLayout>
+            
             {children}
+            
         </AuthLayout>
     )
 }
